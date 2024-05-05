2371dfadcda56496dabf8c0a2b8ae011a5317142
Irbis007 <suskodomk@gmail.com>
2024-05-05 18:56:58 +0300

    fix search

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import Select from "../Select/Select";
 import SelectItem from "../Select/SelectItem/SelectItem";
 import SelectSubitem from "../Select/SelectSubitem/SelectSubitem";
+import { data } from "./data";
 
 import search from "../../assets/search.svg";
 import A1 from "../../assets/level_a1.svg";
@@ -11,10 +13,24 @@ import C1 from "../../assets/level_c1.svg";
 import C2 from "../../assets/level_c2.svg";
 
 import "./style.css";
-import { useState } from "react";
 
 const MainPage = ({ lang }) => {
   const [searchValue, setSearchValue] = useState("");
+  const [searched, setSearched] = useState([]);
+
+  const onSearch = (e) => {
+    const value = e.target.value;
+    const currSearch = value.trim().toLocaleLowerCase();
+
+    setSearched([])
+    data.forEach((item) => {
+      if (currSearch == item.trim().toLowerCase().slice(0, currSearch.length)) {
+
+        setSearched(prev => [...prev, item])
+      }
+    });
+    setSearchValue(value);
+  };
 
   return (
     <div className="main">
@@ -26,7 +42,7 @@ const MainPage = ({ lang }) => {
           className="search__input"
           type="text"
           id="search"
-          onChange={(e) => setSearchValue(e.target.value)}
+          onChange={(e) => onSearch(e)}
           placeholder={lang ? "Поиск" : "Search"}
         />
       </label>
@@ -34,27 +50,33 @@ const MainPage = ({ lang }) => {
       {searchValue.length > 0 ? (
         <div className="show__content">
           <div className="search__content">
-            <span>{searchValue}</span>
-            <label htmlFor={`subitem-a1`} className="subitem__label">
-              <input type="checkbox" id={`subitem-a1`} />
-              <span className="subitem__checkmark"></span>
-              <span className="subitem__content">A1</span>
-            </label>
-            <label htmlFor={`subitem-a2`} className="subitem__label">
-              <input type="checkbox" id={`subitem-a2`} />
-              <span className="subitem__checkmark"></span>
-              <span className="subitem__content">A2</span>
-            </label>
-            <label htmlFor={`subitem-b1`} className="subitem__label">
-              <input type="checkbox" id={`subitem-b1`} />
-              <span className="subitem__checkmark"></span>
-              <span className="subitem__content">B1</span>
-            </label>
-            <label htmlFor={`subitem-b2`} className="subitem__label">
-              <input type="checkbox" id={`subitem-b2`} />
-              <span className="subitem__checkmark"></span>
-              <span className="subitem__content">B2</span>
-            </label>
+            {searched.map((item) => {
+              return (
+                <>
+                  <span>{item}</span>
+                  <label htmlFor={`subitem-a1`} className="subitem__label">
+                    <input type="checkbox" id={`subitem-a1`} />
+                    <span className="subitem__checkmark"></span>
+                    <span className="subitem__content">A1</span>
+                  </label>
+                  <label htmlFor={`subitem-a2`} className="subitem__label">
+                    <input type="checkbox" id={`subitem-a2`} />
+                    <span className="subitem__checkmark"></span>
+                    <span className="subitem__content">A2</span>
+                  </label>
+                  <label htmlFor={`subitem-b1`} className="subitem__label">
+                    <input type="checkbox" id={`subitem-b1`} />
+                    <span className="subitem__checkmark"></span>
+                    <span className="subitem__content">B1</span>
+                  </label>
+                  <label htmlFor={`subitem-b2`} className="subitem__label">
+                    <input type="checkbox" id={`subitem-b2`} />
+                    <span className="subitem__checkmark"></span>
+                    <span className="subitem__content">B2</span>
+                  </label>
+                </>
+              );
+            })}
           </div>
           <a href="https://grammarway.com/ru/demonstrative-pronouns" className="search__link">
             Demonstrative pronouns
@@ -284,7 +306,6 @@ const MainPage = ({ lang }) => {
           <SelectSubitem title={"Compound and Complex Sentences"} />
         </SelectItem>
       </Select>
-     
 
       <button className="button__save">{lang ? "Сохранить" : "Save"}</button>
     </div>
---
+++ src/components/MainPage/data.js
@@ -0,0 +1,24 @@
+
+export const data = [
+  'Present Continuous',
+  'Past Simple',
+  'Present Simple',
+  'Possessive Case',
+  'Prepositions',
+  'Pronouns',
+  'Can и Could',
+  'Dare',
+  'Had better',
+  'Have to',
+  'May и Might',
+  'Must',
+  'Need',
+  'Shall',
+  'Should и Ought to',
+  'To be to',
+  'Will и Would',
+  'Infinitive',
+  'There is / There are',
+  'To be going to',
+  'Compound and Complex Sentences'
+]
